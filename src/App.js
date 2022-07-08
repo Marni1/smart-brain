db93aef185d34604d46438accc48531cc434fc39
Marni1 <rotokwow@gmail.com>
2022-07-09 01:28:03 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ class App extends Component {
   }
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch(`'https://still-badlands-71293.herokuapp.com/imageurl`, {
+    fetch(`https://still-badlands-71293.herokuapp.com/imageurl`, {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -142,7 +142,7 @@ class App extends Component {
 .then(response => response.json())
       .then(response => {
         if(response){
-          fetch(`'https://still-badlands-71293.herokuapp.com/image`, {
+          fetch(`https://still-badlands-71293.herokuapp.com/image`, {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
