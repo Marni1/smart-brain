e97c8806c0d259dcaf85bb08adfde926250c8511
Marcin <71231051+Marni1@users.noreply.github.com>
2023-04-26 17:09:04 +0200

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,6 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
             return(
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
             <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-            <p onClick={() => onRouteChange('leaderboard')} className='f3 link dim black underline pa3 pointer'>Leaderboard</p>
         </nav>
         )
         } else { 
